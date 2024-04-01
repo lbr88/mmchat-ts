da68caab7c83f45c735024ec4b2f80d04f128504
Lars Bo Rasmussen <Lars Bo Rasmussen lrasmussen@aio-it.dk>
2024-04-01 14:19:04 +0000

    if you say so

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('app') as HTMLElement;
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!);
 root.render(<React.StrictMode><App /></React.StrictMode>);
